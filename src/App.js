9e337069a6f7bbc003e70a92a4371e0bc1ab628b
Brandon Fenty <37678434+brandon-fenty@users.noreply.github.com>
2023-12-04 20:14:30 -0800

    fixed

--- src/App.js
+++ src/App.js
@@ -1,90 +1,74 @@
 import React, { useState, useEffect, useRef } from 'react';
-import FlashcardList from './FlashcardList';
-import './App.css'
-import axios from 'axios'
+import './App.css';
+import questions from './data/questions.json';
 
 function App() {
-  const [flashcards, setFlashcards] = useState([])
-  const [categories, setCategories] = useState([])
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [flip, setFlip] = useState(false)
+  const [height, setHeight] = useState('initial')
 
-  const categoryEl = useRef()
-  const amountEl = useRef()
+  const frontEl = useRef()
+  const backEl = useRef()
 
-  useEffect(() => {
-    axios
-      .get('https://opentdb.com/api_category.php')
-      .then(res => {
-        setCategories(res.data.trivia_categories)
-      })
-  }, [])
-
-  useEffect(() => {
-   
-  }, [])
-
-  function decodeString(str) {
-    const textArea = document.createElement('textarea')
-    textArea.innerHTML= str
-    return textArea.value
+  function setMaxHeight() {
+    const frontHeight = frontEl.current.getBoundingClientRect().height
+    const backHeight = backEl.current.getBoundingClientRect().height
+    setHeight(Math.max(frontHeight, backHeight, 100))
   }
+  
+  const flashcards = Object.keys(questions).map(v => ({
+      key: v,
+      ...questions[v]
+  }));
 
-  function handleSubmit(e) {
-    e.preventDefault()
-    axios
-    .get('https://opentdb.com/api.php', {
-      params: {
-        amount: amountEl.current.value,
-        category: categoryEl.current.value
-      }
-    })
-    .then(res => {
-      setFlashcards(res.data.results.map((questionItem, index) => {
-        const answer = decodeString(questionItem.correct_answer)
-        const options = [
-          ...questionItem.incorrect_answers.map(a => decodeString(a)),
-          answer
-        ]
-        return {
-          id: `${index}-${Date.now()}`,
-          question: decodeString(questionItem.question),
-          answer: answer,
-          options: options.sort(() => Math.random() - .5)
-        }
-      }))
-    })
-  }
+  const handleNextCard = () => {
+    setCurrentCardIndex((prevIndex) => (prevIndex + 1) % flashcards.length);
+  };
+
+  const handlePrevCard = () => {
+    setCurrentCardIndex((prevIndex) => (prevIndex - 1 + flashcards.length) % flashcards.length);
+  };
+
+  useEffect(setMaxHeight, [
+    flashcards[currentCardIndex].image_url,
+    flashcards[currentCardIndex].question,
+    flashcards[currentCardIndex].answer])
+  useEffect(() => {
+    window.addEventListener('resize', setMaxHeight)
+    return () => window.removeEventListener('resize', setMaxHeight)
+  }, [])
 
   return (
-    <>
-      <div>
-        
-        <form className="header" onSubmit={handleSubmit}>
+    <div className="app">
+      <div className='header'>
         <div className='title'>
-          <h1>Dendrocabulary</h1>
-          <h2 className='sub-title'>A Study Aid for Tree and Shrub Identification</h2>
-        </div>
-          <div className="form-group">
-            <label htmlFor="category">Category</label>
-            <select id="category" ref={categoryEl}>
-              {categories.map(category => {
-                return <option value={category.id} key={category.id}>{category.name}</option>
-              })}
-            </select>
-          </div>
-          <div className="form-group">
-            <label htmlFor="amount">Number of Questions</label>
-            <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
-          </div>
-          <div className="form-group">
-            <button className="btn">Generate</button>
-          </div>
-        </form>
-        <div className="container">
-          <FlashcardList flashcards={flashcards} />
+            <h1>Dendrocabulary</h1>
+            <h2 className='sub-title'>A Study Aid for Tree and Shrub Identification</h2>
         </div>
       </div>
-    </>
+      {flashcards.length > 0 && (
+        <div className='container'>
+          <div className= {`card ${flip ? 'flip' : ''}`}
+            style={{ height: height }}
+            onClick={() => setFlip(!flip)}
+          >
+              <div className='front' ref={frontEl}>
+                {/* <image url={flashcards[currentCardIndex].image.url} /> */}
+                {flashcards[currentCardIndex].question}
+              </div>
+              <div className='back' ref={backEl}>
+                {/* <image url={flashcards[currentCardIndex].image.url} /> */}
+                {flashcards[currentCardIndex].answer}
+              </div>
+          </div>
+        </div>
+      )}
+      <div className="button-container">
+        <button className='btn' onClick={handlePrevCard}>Previous</button>
+        <button className='btn' onClick={handleNextCard}>Next</button>
+      </div>
+    </div>
   );
-}
+};
 
 export default App;
